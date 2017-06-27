7f310022f21faa5123bba2dfb44f0ab2291da44e
Frank Henard <fdhenard@gmail.com>
2017-06-27 12:13:08 -0500

    dev0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="raygun-django-middleware",
-    version="1.0.3.dev",
+    version="1.0.3.dev0",
     description="Raygun Django Middleware",
     author="Mirus Research",
     author_email="frank@mirusresearch.com",
